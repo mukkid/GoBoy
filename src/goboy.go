b4618d68e50a84fdc95ab5bdcaa5987441c22ead
mukkid <mukkid@gmail.com>
2018-02-25 03:19:05 -0600

    added setters for registers (#7)
    * added setters for registers
    
    * refactored getters and setters
    
    * Removed high/low byte funcs

--- src/goboy.go
+++ src/goboy.go
@@ -7,62 +7,60 @@ type GameBoy struct {
     vram [8 * 1024]uint8
 }
 
+type Reg8ID int
+type Reg16ID int
+
+const (
+    A Reg8ID = 0
+    F Reg8ID = 1
+    B Reg8ID = 2
+    C Reg8ID = 3
+    D Reg8ID = 4
+    E Reg8ID = 5
+    H Reg8ID = 6
+    L Reg8ID = 7
+)
+
+const (
+    AF Reg16ID = 0
+    BC Reg16ID = 1
+    DE Reg16ID = 2
+    HL Reg16ID = 3
+    SP Reg16ID = 4
+    PC Reg16ID = 5
+)
+
+
 type Register struct {
     regs [6]uint16
 }
 
-func (r *Register) getA() uint8 {
-    return uint8(r.regs[0] >> 8)
+func (r *Register) get16Reg(id Reg16ID) uint16 {
+    return r.regs[id]
 }
 
-func (r *Register) getF() uint8 {
-    return uint8(r.regs[0])
+func (r *Register) set16Reg(id Reg16ID, value uint16) {
+    r.regs[id] = value
 }
 
-func (r *Register) getB() uint8 {
-    return uint8(r.regs[1] >> 8)
+func (r *Register) get8Reg(id Reg8ID) uint8 {
+    block := id / 2  // block is the index of the 16bit version
+    end := id % 2  // end indicates the high (0) or low (1) byte of the 16bit register
+    if end == 0 {
+        return uint8(r.regs[block] >> 8)
+    } else {
+        return uint8(r.regs[block])
+    }
 }
 
-func (r *Register) getC() uint8 {
-    return uint8(r.regs[1])
-}
-
-func (r *Register) getD() uint8 {
-    return uint8(r.regs[2] >> 8)
-}
-
-func (r *Register) getE() uint8 {
-    return uint8(r.regs[2])
-}
-
-func (r *Register) getH() uint8 {
-    return uint8(r.regs[3] >> 8)
-}
-
-func (r *Register) getL() uint8 {
-    return uint8(r.regs[3])
-}
-
-func (r *Register) getSP() uint16 {
-    return r.regs[4]
-}
-
-func (r *Register) getPC() uint16 {
-    return r.regs[5]
-}
-
-func (r *Register) getAF() uint16 {
-    return r.regs[0]
-}
-
-func (r *Register) getBC() uint16 {
-    return r.regs[1]
-}
-
-func (r *Register) getDE() uint16 {
-    return r.regs[2]
-}
-
-func (r *Register) getHL() uint16 {
-    return r.regs[3]
+func (r *Register) set8Reg(id Reg8ID, value uint8) {
+    block := id / 2
+    end := id % 2
+    if end == 0 {
+        r.regs[block] &= 0x00ff
+        r.regs[block] |= uint16(value << 8)
+    } else {
+        r.regs[block] &= 0xff00
+        r.regs[block] |= uint16(value)
+    }
 }
