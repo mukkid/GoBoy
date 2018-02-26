b71ec8c068b555a85acdb096d0b08f9282928de0
Mukund Kidambi <mukkid@gmail.com>
2018-02-26 17:59:57 -0600

    Added next LD instruction/updated gitignore for vim swp files

--- src/goboy/opcodes_test.go
+++ src/goboy/opcodes_test.go
@@ -7,7 +7,7 @@ func TestLD_r_r(t *testing.T) {
         Register: &Register{},
     }
     gb.set8Reg(B, 0xfe)
-    gb.LD_r_r(0x50) // instruction for LD D, B
+    gb.LD_r_r(0x50) // LD D, B
     if gb.get8Reg(B) != 0xfe {
         t.Fatal()
     }
@@ -15,6 +15,19 @@ func TestLD_r_r(t *testing.T) {
         t.Fatal()
     }
     if gb.get16Reg(PC) != 0x0001 {
+        t.Fatal()
+    }
+}
+
+func TestLD_r_n(t *testing.T) {
+    gb := &GameBoy{
+        Register: &Register{},
+    }
+    gb.LD_r_n(0xead) // LD C, 0xad
+    if gb.get8Reg(C) != 0xad {
+        t.Fatal()
+    }
+    if gb.get16Reg(PC) != 0x0002 {
         t.Fatal()
     }
 }
