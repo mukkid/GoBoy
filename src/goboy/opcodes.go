14c03376d4a55e491b420db20c9a389027ffde14
Mukund Kidambi <mukkid@gmail.com>
2018-02-26 22:49:00 -0600

    Removed increment function

--- src/goboy/opcodes.go
+++ src/goboy/opcodes.go
@@ -5,7 +5,7 @@ func (gb *GameBoy) LD_r_r(ins uint8) {
     r1 := Reg8ID((ins & 0x38) >> 3)
     r2 := Reg8ID(ins & 0x07)
     gb.set8Reg(r1, gb.get8Reg(r2))
-    gb.incrementPc(1)
+    gb.regs[PC] += 1
 }
 
 // Load r <- n
@@ -13,5 +13,14 @@ func (gb *GameBoy) LD_r_n(ins uint16) {
     n := uint8(ins)
     r := Reg8ID((ins & 0x3800) >> 11)
     gb.set8Reg(r, n)
-    gb.incrementPc(2)
+    gb.regs[PC] += 2
+}
+
+// Load r <- (HL)
+// TODO: Write unit test
+func (gb *GameBoy) LD_r_hl(ins uint8) {
+    r := Reg8ID((ins & 0x38) >> 3)
+    address := gb.get16Reg(HL)
+    gb.set8Reg(r, gb.mainMemory.read(address))
+    gb.regs[PC] += 1
 }
