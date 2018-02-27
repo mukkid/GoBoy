f2c5e7ce7e00ff1b679ec0f7e05434c9a25675a5
Mukund Kidambi <mukkid@gmail.com>
2018-02-26 23:41:47 -0600

    modified tests to use assert

--- src/goboy/goboy_test.go
+++ src/goboy/goboy_test.go
@@ -1,34 +1,29 @@
 package GoBoy
 
-import "testing"
+import (
+    "testing"
+    "github.com/stretchr/testify/assert"
+)
 
 func TestGet16Reg(t *testing.T) {
     regs := &Register{}
-    if regs.get16Reg(BC) != 0x00 {
-        t.Fatal()
-    }
+    assert.Equal(t, regs.get16Reg(BC), uint16(0x00))
 }
 
 func TestSet16Reg(t *testing.T) {
     regs := &Register{}
     regs.set16Reg(BC, 0xbeef)
-    if regs.get16Reg(BC) != 0xbeef {
-        t.Fatal()
-    }
+    assert.Equal(t, regs.get16Reg(BC), uint16(0xbeef))
 }
 
 func TestGet8Reg(t *testing.T) {
     regs := &Register{}
     regs.set16Reg(BC, 0xfeed)
-    if regs.get8Reg(C) != 0xed {
-        t.Fatal()
-    }
+    assert.Equal(t, regs.get8Reg(C), uint8(0xed))
 }
 
 func TestSet8Reg(t *testing.T) {
     regs := &Register{}
     regs.set8Reg(A, 0xde)
-    if regs.get8Reg(A) != 0xde {
-        t.Fatal()
-    }
+    assert.Equal(t, regs.get8Reg(A), uint8(0xde))
 }
--- src/goboy/opcodes_test.go
+++ src/goboy/opcodes_test.go
@@ -1,6 +1,9 @@
 package GoBoy
 
-import "testing"
+import (
+    "testing"
+    "github.com/stretchr/testify/assert"
+)
 
 func TestLD_r_r(t *testing.T) {
     gb := &GameBoy{
@@ -8,15 +11,9 @@ func TestLD_r_r(t *testing.T) {
     }
     gb.set8Reg(B, 0xfe)
     gb.LD_r_r(0x50) // LD D, B
-    if gb.get8Reg(B) != 0xfe {
-        t.Fatal()
-    }
-    if gb.get8Reg(D) != 0xfe {
-        t.Fatal()
-    }
-    if gb.get16Reg(PC) != 0x0001 {
-        t.Fatal()
-    }
+    assert.Equal(t, gb.get8Reg(B), uint8(0xfe))
+    assert.Equal(t, gb.get8Reg(D), uint8(0xfe))
+    assert.Equal(t, gb.get16Reg(PC), uint16(0x0001))
 }
 
 func TestLD_r_n(t *testing.T) {
@@ -24,10 +21,6 @@ func TestLD_r_n(t *testing.T) {
         Register: &Register{},
     }
     gb.LD_r_n(0xead) // LD C, 0xad
-    if gb.get8Reg(C) != 0xad {
-        t.Fatal()
-    }
-    if gb.get16Reg(PC) != 0x0002 {
-        t.Fatal()
-    }
+    assert.Equal(t, gb.get8Reg(C), uint8(0xad)) 
+    assert.Equal(t, gb.get16Reg(PC), uint16(0x0002))
 }
