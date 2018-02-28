96652643eb5523825b11e51875fcf6b468c6ca73
Danny Chen <dannychen579@utexas.edu>
2018-02-28 00:31:23 -0800

    Started on a z80 disassembler POC. Implemented for a few instructions

---
+++ src/gobjdump/main.go
@@ -0,0 +1,30 @@
+package main
+
+import (
+    "os"
+    "bufio"
+    "fmt"
+)
+
+func main_c(argv []string) int {
+    if len(argv) < 2 {
+        fmt.Printf("Usage: %s <binary>\n", argv[0])
+        return 1
+    }
+
+    /* Open binary */
+    file, err := os.Open(argv[1])
+    if err != nil {
+        fmt.Printf("%s\n", err.Error())
+        return 1
+    }
+
+    bufReader := bufio.NewReader(file)
+    disassemblerLoop(bufReader)
+
+    return 0
+}
+
+func main() {
+    os.Exit(main_c(os.Args))
+}
