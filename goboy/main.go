b04ba17448b2a0825bcf0df4fe4a474502408dc5
Mukund Kidambi <mukkid@gmail.com>
2018-04-29 15:41:52 -0500

    Added rate limiting and merged gpu and cpu functions

--- goboy/main.go
+++ goboy/main.go
@@ -2,9 +2,9 @@ package main
 
 import (
 	"flag"
-	"fmt"
 	"image"
 	"image/color"
+	"time"
 
 	"github.com/faiface/pixel"
 	"github.com/faiface/pixel/pixelgl"
@@ -32,28 +32,6 @@ func run() {
 		panic(err)
 	}
 
-	d := &pixel.PictureData{
-		Pix:    make([]color.RGBA, 144*160),
-		Stride: 160,
-		Rect:   pixel.R(0, 0, 160, 144),
-	}
-
-	for i := 0; i < 144*10; i++ {
-		d.Pix[i] = color.RGBA{R: 0xff, G: 0x00, B: 0x00, A: 0xff}
-		d.Pix[144*160-i-1] = color.RGBA{R: 0xff, G: 0x00, B: 0x00, A: 0xff}
-	}
-	for !win.Closed() {
-		win.Clear(color.RGBA{R: 0x00, G: 0x00, B: 0x00, A: 0xff})
-		sprite := pixel.NewSprite(pixel.Picture(d), pixel.R(0, 0, 160, 144))
-		mat := pixel.IM.Scaled(pixel.ZV, 3)
-		mat = mat.Moved(win.Bounds().Center())
-		sprite.Draw(win, mat)
-
-		win.Update()
-	}
-}
-
-func main() {
 	// init gameboy
 	Gb = &GameBoy{
 		Register:         &Register{},
@@ -68,12 +46,40 @@ func main() {
 	if *rom_path != "" {
 		Gb.mainMemory.cartridge.loadROMFromFile(*rom_path)
 	}
-	fmt.Println(Gb.rom)
-	fmt.Println(Gb.regs)
 
-	pixelgl.Run(run)
-	for true {
+	d := &pixel.PictureData{
+		Pix:    make([]color.RGBA, 144*160),
+		Stride: 160,
+		Rect:   pixel.R(0, 0, 160, 144),
+	}
+
+	for i := 0; i < 144*10; i++ {
+		d.Pix[i] = color.RGBA{R: 0xff, G: 0x00, B: 0x00, A: 0xff}
+		d.Pix[144*160-i-1] = color.RGBA{R: 0xff, G: 0x00, B: 0x00, A: 0xff}
+	}
+
+	clock := time.Now()
+	var tick time.Time
+	for !win.Closed() {
 		Gb.handleInterrupts()
 		Gb.Step()
+
+		// Enfore refresh rate of 60hz
+		tick = time.Now()
+		if tick.Sub(clock) > time.Duration(16660000) {
+			win.Clear(color.RGBA{R: 0x00, G: 0x00, B: 0x00, A: 0xff})
+			mat := pixel.IM.Scaled(pixel.ZV, 3)
+			mat = mat.Moved(win.Bounds().Center())
+			// Sprite needs to be contructed from the tilemap
+			sprite := pixel.NewSprite(pixel.Picture(d), pixel.R(0, 0, 160, 144))
+			sprite.Draw(win, mat)
+
+			win.Update()
+			clock = tick
+		}
 	}
+}
+
+func main() {
+	pixelgl.Run(run)
 }
